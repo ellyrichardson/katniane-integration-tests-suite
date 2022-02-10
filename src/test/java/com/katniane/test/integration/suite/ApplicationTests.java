19cceb602611a80b161246e75dd87fa15965da66
Elly Richardson <elly.richardson18@gmail.com>
2022-02-09 18:00:58 -0600

    First commit

---
+++ src/test/java/com/katniane/test/integration/suite/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katniane.test.integration.suite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
